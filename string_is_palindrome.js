2c97c90c6e419ed4f50c7bdb66db4b68b7fec464
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-04-16 00:56:21 +0430

    string is palindrome

---
+++ string_is_palindrome.js
@@ -0,0 +1,30 @@
+/*
+  Task: Remove all duplicate items from the given array
+  Input:
+    const firstString = "madam"
+    const secondString = "dog"
+  Output:
+    firstString => true (is palindrome)
+    secondString => false (is NOT palindrome)
+*/
+
+const firstString = "Madam";
+const secondString = "dog";
+
+/*
+  The "split()" method divides a string into an ordered list of substrings, puts these substrings into an array...
+  The "reverse()" method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.
+  The "join" method creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string.
+  ref1: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+  ref2: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+  ref3: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+*/
+const splittedFirstString = firstString.split("");
+const reversedFirstStringArray = splittedFirstString.reverse();
+const reversedFirstString = reversedFirstStringArray.join("");
+console.log(reversedFirstString.toLowerCase() === firstString.toLowerCase());
+
+const splittedSecondString = secondString.split("");
+const reversedSecondStringArray = splittedSecondString.reverse();
+const reversedSecondString = reversedSecondStringArray.join("");
+console.log(reversedSecondString.toLowerCase() === secondString.toLowerCase());
