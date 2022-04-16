b9572557b1dde2c8224a8db3afccb4b6089db8df
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-04-16 16:35:29 +0430

    Add solution description

--- string_is_palindrome.js
+++ string_is_palindrome.js
@@ -12,6 +12,7 @@ const firstString = "Madam";
 const secondString = "dog";
 
 /*
+  Solution: Divide string into array of chars, reverse the array, and concatenate chars array into a string!
   The "split()" method divides a string into an ordered list of substrings, puts these substrings into an array...
   The "reverse()" method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.
   The "join" method creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string.
